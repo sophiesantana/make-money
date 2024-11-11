f4ad48d17a6839013f0ea68256e250971cc80562
Sophie Santana <ssophiesantana@gmail.com>
2024-11-11 19:27:21 -0300

    chore(modules): integracao do typeorm com postgres

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Make Money')
+    .setDescription('Desenvolvimento API transferencia de saldo')
+    .setVersion('1.0')
+    .addTag('Transferencia de Saldo')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
